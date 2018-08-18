d2ebd58c5cb1df8780f3193408b2ebdcee6c7d9c
Merlin <merlinb1992@gmail.com>
2018-08-18 12:08:28 +0200

    add basic currency dropdown for the currency node generation form

--- src/components/CurrencyForm.vue
+++ src/components/CurrencyForm.vue
@@ -1,10 +1,40 @@
 <template>
-	<h3>Currency Form</h3>
+	<el-container>
+		<el-select v-model="emptyValue" filterable placeholder="Select">
+			<el-option
+					v-for="item in options"
+					:key="item.value"
+					:label="item.label"
+					:value="item.value">
+			</el-option>
+		</el-select>
+	</el-container>
 </template>
 
 <script>
 	export default {
-		name: 'CurrencyForm'
+		name: 'CurrencyForm',
+		data() {
+			return {
+				options: [{
+					value: 'Option1',
+					label: 'Chaos Orb'
+				}, {
+					value: 'Option2',
+					label: 'Exalted Orb'
+				}, {
+					value: 'Option3',
+					label: 'Chromatic Orb'
+				}, {
+					value: 'Option4',
+					label: 'Alchemy Orb'
+				}, {
+					value: 'Option5',
+					label: 'Orb of Fusing'
+				}],
+				emptyValue: ''
+			}
+		}
 	}
 </script>
 
