f8902da727fd1ca8e87e3e4f29e272f8ce3f6404
Merlin <merlinb1992@gmail.com>
2018-08-28 19:53:50 +0200

    moved option values into the form data and add onSubmit method

--- src/components/CurrencyForm.vue
+++ src/components/CurrencyForm.vue
@@ -48,142 +48,148 @@
 <script>
 	export default {
 		name: 'CurrencyForm',
+		methods: {
+			onSubmit: function () {
+			}
+		},
 		data() {
 			return {
-				options: [{
-					value: 'blesse',
-					label: 'Blessed Orb'
-				}, {
-					value: 'chisel',
-					label: 'Cartographer\'s Chisel'
-				}, {
-					value: 'chrom',
-					label: 'Chromatic Orb'
-				}, {
-					value: 'chaos',
-					label: 'Chaos Orb'
-				}, {
-					value: 'divine',
-					label: 'Divine Orb'
-				}, {
-					value: 'exalted',
-					label: 'Exalted Orb'
-				}, {
-					value: 'gcp',
-					label: 'Gemcutter\'s Prism'
-				}, {
-					value: 'jew',
-					label: 'Jeweller\'s Orb'
-				}, {
-					value: 'alchemy',
-					label: 'Orb of Alchemy'
-				}, {
-					value: 'alt',
-					label: 'Orb of Alteration'
-				}, {
-					value: 'chance',
-					label: 'Orb of Chance'
-				}, {
-					value: 'fuse',
-					label: 'Orb of Fusing'
-				}, {
-					value: 'regret',
-					label: 'Orb of Regret'
-				}, {
-					value: 'scour',
-					label: 'Orb of Scouring'
-				}, {
-					value: 'regal',
-					label: 'Regal Orb'
-				}, {
-					value: 'vaal',
-					label: 'Vaal Orb'
-				}, {
-					value: 'perandus',
-					label: 'Perandus Coin'
-				}, {
-					value: 'silver',
-					label: 'Silver Coin'
-				}, {
-					value: 'glass',
-					label: 'Glassblower\'s Bauble'
-				}, {
-					value: 'aug',
-					label: 'Orb of Augmentation'
-				}, {
-					value: 'mir',
-					label: 'Mirror of Kalandra'
-				}, {
-					value: 'tra',
-					label: 'Orb of Transmutation'
-				}, {
-					value: 'blacksmith',
-					label: 'Blacksmith\'s Whetstone'
-				}, {
-					value: 'armour',
-					label: 'Armourer\'s Scrap'
-				}, {
-					value: 'wis',
-					label: 'Scroll of Wisdom'
-				}, {
-					value: 'port',
-					label: 'Portal Scroll'
-				}, {
-					value: 'ete',
-					label: 'Eternal Orb'
-				}, {
-					value: 'offer',
-					label: 'Offering to the Goddess'
-				}, {
-					value: 'dusk',
-					label: 'Sacrifice at Dusk'
-				}, {
-					value: 'midnight',
-					label: 'Sacrifice at Midnight'
-				}, {
-					value: 'dawn',
-					label: 'Sacrifice at Dawn'
-				}, {
-					value: 'noon',
-					label: 'Sacrifice at Noon'
-				}, {
-					value: 'grie',
-					label: 'Mortal Grief'
-				}, {
-					value: 'rage',
-					label: 'Mortal Rage'
-				}, {
-					value: 'hope',
-					label: 'Mortal Hope'
-				}, {
-					value: 'ign',
-					label: 'Mortal Ignorance'
-				}, {
-					value: 'hydra',
-					label: 'Fragment of the Hydra'
-				}, {
-					value: 'phoenix',
-					label: 'Fragment of the Phoenix'
-				}, {
-					value: 'minot',
-					label: 'Fragment of the Minotaur'
-				}, {
-					value: 'chimer',
-					label: 'Fragment of the Chimera'
-				}, {
-					value: 'apprentice-sextant',
-					label: 'Apprentice Cartographer\'s Sextant'
-				}, {
-					value: 'journeyman-sextant',
-					label: 'Journeyman Cartographer\'s Sextant'
-				}, {
-					value: 'master-sextant',
-					label: 'Master Cartographer\'s Sextant'
-				}],
 				emptySellValue: '',
 				emptyBuyValue: '',
 				sellAmount: '',
-				buyAmount: ''
+				buyAmount: '',
+				form: {
+					options: [{
+						value: 'blesse',
+						label: 'Blessed Orb'
+					}, {
+						value: 'chisel',
+						label: 'Cartographer\'s Chisel'
+					}, {
+						value: 'chrom',
+						label: 'Chromatic Orb'
+					}, {
+						value: 'chaos',
+						label: 'Chaos Orb'
+					}, {
+						value: 'divine',
+						label: 'Divine Orb'
+					}, {
+						value: 'exalted',
+						label: 'Exalted Orb'
+					}, {
+						value: 'gcp',
+						label: 'Gemcutter\'s Prism'
+					}, {
+						value: 'jew',
+						label: 'Jeweller\'s Orb'
+					}, {
+						value: 'alchemy',
+						label: 'Orb of Alchemy'
+					}, {
+						value: 'alt',
+						label: 'Orb of Alteration'
+					}, {
+						value: 'chance',
+						label: 'Orb of Chance'
+					}, {
+						value: 'fuse',
+						label: 'Orb of Fusing'
+					}, {
+						value: 'regret',
+						label: 'Orb of Regret'
+					}, {
+						value: 'scour',
+						label: 'Orb of Scouring'
+					}, {
+						value: 'regal',
+						label: 'Regal Orb'
+					}, {
+						value: 'vaal',
+						label: 'Vaal Orb'
+					}, {
+						value: 'perandus',
+						label: 'Perandus Coin'
+					}, {
+						value: 'silver',
+						label: 'Silver Coin'
+					}, {
+						value: 'glass',
+						label: 'Glassblower\'s Bauble'
+					}, {
+						value: 'aug',
+						label: 'Orb of Augmentation'
+					}, {
+						value: 'mir',
+						label: 'Mirror of Kalandra'
+					}, {
+						value: 'tra',
+						label: 'Orb of Transmutation'
+					}, {
+						value: 'blacksmith',
+						label: 'Blacksmith\'s Whetstone'
+					}, {
+						value: 'armour',
+						label: 'Armourer\'s Scrap'
+					}, {
+						value: 'wis',
+						label: 'Scroll of Wisdom'
+					}, {
+						value: 'port',
+						label: 'Portal Scroll'
+					}, {
+						value: 'ete',
+						label: 'Eternal Orb'
+					}, {
+						value: 'offer',
+						label: 'Offering to the Goddess'
+					}, {
+						value: 'dusk',
+						label: 'Sacrifice at Dusk'
+					}, {
+						value: 'midnight',
+						label: 'Sacrifice at Midnight'
+					}, {
+						value: 'dawn',
+						label: 'Sacrifice at Dawn'
+					}, {
+						value: 'noon',
+						label: 'Sacrifice at Noon'
+					}, {
+						value: 'grie',
+						label: 'Mortal Grief'
+					}, {
+						value: 'rage',
+						label: 'Mortal Rage'
+					}, {
+						value: 'hope',
+						label: 'Mortal Hope'
+					}, {
+						value: 'ign',
+						label: 'Mortal Ignorance'
+					}, {
+						value: 'hydra',
+						label: 'Fragment of the Hydra'
+					}, {
+						value: 'phoenix',
+						label: 'Fragment of the Phoenix'
+					}, {
+						value: 'minot',
+						label: 'Fragment of the Minotaur'
+					}, {
+						value: 'chimer',
+						label: 'Fragment of the Chimera'
+					}, {
+						value: 'apprentice-sextant',
+						label: 'Apprentice Cartographer\'s Sextant'
+					}, {
+						value: 'journeyman-sextant',
+						label: 'Journeyman Cartographer\'s Sextant'
+					}, {
+						value: 'master-sextant',
+						label: 'Master Cartographer\'s Sextant'
+					}]
+				}
 			}
 		}
 	}
